ce8e099a4573e22271f4fb8a68d58c2f10425ca2
EnzoSantoss <enzoaugusto312@gmail.com>
2024-02-21 17:37:31 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-components/new-components.component';
 import { TesteTemplateComponent } from './components/template/teste-template/teste-template.component';
+import { PageGoodComponent } from './components/template/page-good/page-good.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TesteTemplateComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    TesteTemplateComponent,
+    PageGoodComponent,
+  ],
   template: `
     <!-- <router-outlet></router-outlet> -->
     <!-- <app-new-components /> -->
-    <app-teste-template />
+    <!-- <app-teste-template /> -->
+    <app-page-good />
   `,
 })
 export class AppComponent {
---
+++ src/app/components/template/page-good/page-good.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-good',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-good.component.html',
+  styleUrl: './page-good.component.scss'
+})
+export class PageGoodComponent {
+
+}
